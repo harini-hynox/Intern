14700b810de83dbb3698bc4689f2c27fa3e5131f
Harini Kandasamy <harinikandasamy.thehynox@gmail.com>
2025-09-01 14:24:30 +0530

    task1_initial one

---
+++ sample/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Welcome_page from './pages/Welcome_page';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome_page />
+    </div>
+  );
+}
+
+export default App;
---
+++ sample/src/pages/Welcome_page.js
@@ -0,0 +1,20 @@
+import React  from 'react'
+import './../styles/Welcome_page.css';
+
+
+const Welcome_page = () => {
+  
+  return (
+    <div className='wp_main'>
+        <div className='wp_nav'>
+            <h4>HOME</h4>
+            <h4>ABOUT</h4>
+            <h4>CAREERS</h4>
+            <h4>CONTACT</h4>
+        </div>
+        <h1 className='wp_content'>WELCOME HYNOX</h1>
+    </div>
+  )
+}
+
+export default Welcome_page
